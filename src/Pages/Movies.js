6e8adde555c5cf0289f7198e58286d8f6f59b953
bola-nabil <bolanabil404@gmail.com>
2024-03-19 13:29:45 +0200

    update Movies Page

--- src/Pages/Movies.js
+++ src/Pages/Movies.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 import {useState, useEffect} from "react";
-import SingleContent from "../components/SingleContent";
 import CustomPagination from "../components/CustomPagination";
 import Genres from "../components/Geners";
 import useGenre from "../hooks/useGenre";
-
+import ContentModal from "../components/ContentModal";
 
 function Movies() {
     const [genres, setGenres] = useState([]);
@@ -17,10 +16,9 @@ function Movies() {
     const fetchMovies = async () => {
         const { data } = await axios.get(
             `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreForURL}`
-        )
+        );
 
         setContent(data.results);
-        console.log(data);
         setNumOfPages(data.total_pages);
     }
 
@@ -43,15 +41,15 @@ function Movies() {
         />
         <div className="trending">
           {content &&
-            content.map((item) => (
-              <SingleContent
-                key={item.id}
-                id={item.id}
+            content?.map((item) => (
+              <ContentModal
+                key={item?.id}
+                id={item?.id}
                 poster={item.poster_path}
-                title={item.title || item.name}
-                date={item.first_air_date || item.release_date}
+                title={item?.title || item?.name}
+                date={item?.first_air_date || item?.release_date}
                 media_type="movie"
-                vote_average={item.vote_average}
+                vote_average={item?.vote_average}
               />
             ))}
         </div>
