2fe66c3954e74e275ab6e0de79455122eaa55939
bola-nabil <bolanabil404@gmail.com>
2024-03-19 19:06:57 +0200

    Edit Font SIze

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -2,7 +2,7 @@ function NotFound() {
     return (
       <>
         <div className="d-flex justify-content-center align-items-center not-found">
-          <p className="fs-2 text-light fw-bold">Sorry! This Page Not Found</p>
+          <p className="fs-3 text-light fw-bold">Sorry! This Page Not Found</p>
         </div>
       </>
     );
